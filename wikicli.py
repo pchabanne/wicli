cbbe7791c439a3529cdd208b90dea8fcee0b0871
pchabanne <pablo.chabanne@gmail.com>
2021-09-28 09:31:03 +0200

    random page can be in an other language

--- wikicli.py
+++ wikicli.py
@@ -10,12 +10,14 @@ parser.add_argument("--lang", "-l", type=str,  help="choose language")
 parser.add_argument("--full", "-f", action="store_true", help="if sets, display the full page")
 args = parser.parse_args()
 
+lang = "en"
 
 if args.lang:
 	wikipedia.set_lang(args.lang)
+	lang = args.lang
 
 if args.search == "random":
-	url = "https://en.wikipedia.org/api/rest_v1/page/random/title"
+	url = "https://"+lang+".wikipedia.org/api/rest_v1/page/random/title"
 	req = urllib.request.Request(url)
 	r = urllib.request.urlopen(req).read()
 	cont = json.loads(r.decode("utf-8"))
